17cf55d7ef1271908fe7b75a28117368d7062d81
alfredjynx <99195331+alfredjynx@users.noreply.github.com>
2023-04-24 15:25:48 -0300

    jelado

--- src/part_03/FrozenLake100times.py
+++ src/part_03/FrozenLake100times.py
@@ -5,13 +5,17 @@ from numpy import loadtxt
 env = gym.make('FrozenLake-v1', map_name='8x8', render_mode='ansi').env
 q_table = loadtxt('data/q-table-frozen-lake-qlearning.csv', delimiter=',')
 
-rewards = 0
+r = list()
 
-for i in range(0,100):    
-    (state, _) = env.reset()
-    done = False
-    while not done:
-        action = np.argmax(q_table[state])
-        state, reward, done, _, info = env.step(action)
-    rewards += reward
-print(rewards)
+for _ in range(100):
+    rewards = 0
+    for i in range(0,100):    
+        (state, _) = env.reset()
+        done = False
+        while not done:
+            action = np.argmax(q_table[state])
+            state, reward, done, _, info = env.step(action)
+        rewards += reward
+    r.append(rewards)
+
+print(np.mean(r))
